54bcd548ec061ed74ddf0631df83740b06bd437b
HOUSSAT99 <zhoussat@gmail.com>
2025-05-20 21:36:23 +0100

    Add files via upload

---
+++ transport_db.sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2025 at 06:39 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `transport`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `abcd` (IN `push` INT)  NO SQL SELECT* from user_info$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `nishu` (IN `bus` INT)  NO SQL SELECT* from bus_details$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `raj` (IN `raj` INT)  NO SQL SELECT* from ticket$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `a_id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `psw` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`a_id`, `name`, `psw`) VALUES
+(1, 'Zakaria ', 'zak1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_det`
+--
+
+CREATE TABLE `booking_det` (
+  `bus_id` int(11) NOT NULL,
+  `vacant` int(11) NOT NULL,
+  `jdate` varchar(30) NOT NULL,
+  `bfrom` varchar(30) NOT NULL,
+  `bto` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `booking_det`
+--
+
+INSERT INTO `booking_det` (`bus_id`, `vacant`, `jdate`, `bfrom`, `bto`) VALUES
+(3, 75, '2025-05-16', 'Dublin ', 'Galway'),
+(2, 78, '2019-11-21', 'Dublin ', 'Limerick');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bus_details`
+--
+
+CREATE TABLE `bus_details` (
+  `bus_id` int(11) NOT NULL,
+  `bname` varchar(30) NOT NULL,
+  `bno` varchar(20) NOT NULL,
+  `bfrom` varchar(30) NOT NULL,
+  `bto` varchar(30) NOT NULL,
+  `time` varchar(10) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `no_seat` int(11) NOT NULL,
+  `fare` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `bus_details`
+--
+
+INSERT INTO `bus_details` (`bus_id`, `bname`, `bno`, `bfrom`, `bto`, `time`, `type`, `no_seat`, `fare`) VALUES
+(1, 'Dublin Express', '17D1234', 'dublin', 'galway', '6pm', 'Ac', 20, 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `tid` int(11) NOT NULL,
+  `bus_id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `seat_no` varchar(30) NOT NULL,
+  `no_seat` int(11) NOT NULL,
+  `ticket_status` varchar(30) NOT NULL,
+  `jdate` varchar(30) NOT NULL,
+  `booking_date` date NOT NULL,
+  `pname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`tid`, `bus_id`, `uid`, `seat_no`, `no_seat`, `ticket_status`, `jdate`, `booking_date`, `pname`) VALUES
+(1, 1, 1, '10', 2, 'Conform', '2025-05-16', '2025-05-16', 'zak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `uid` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `uname` varchar(30) NOT NULL,
+  `age` varchar(30) NOT NULL,
+  `adhar_no` varchar(30) NOT NULL,
+  `psw` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`uid`, `name`, `uname`, `age`, `adhar_no`, `psw`, `email`) VALUES
+(1, 'zakaria', 'houssat', '25', '123456', 'zak1234', 'zhoussat@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`a_id`);
+
+--
+-- Indexes for table `bus_details`
+--
+ALTER TABLE `bus_details`
+  ADD PRIMARY KEY (`bus_id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bus_details`
+--
+ALTER TABLE `bus_details`
+  MODIFY `bus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
